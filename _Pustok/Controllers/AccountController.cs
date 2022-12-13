bf22be9b9748c66e8ed890a2f7f0b16bf1eb31dd
Safaroff90 <110519574+Safaroff90@users.noreply.github.com>
2022-12-14 02:06:54 +0400

    member Login

---
+++ _Pustok/Controllers/AccountController.cs
@@ -0,0 +1,142 @@
+﻿using _Pustok.DAL;
+using _Pustok.Models;
+using _Pustok.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace _Pustok.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly PustokContext _pustokContext;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(PustokContext pustokContext,UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this._pustokContext = pustokContext;
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(MemberRegisterViewModel memberVM)
+        {
+            if(!ModelState.IsValid)
+                return View();
+
+            //AppUser user = _pustokContext.AppUsers.FirstOrDefault(x => x.NormalizedUserName == memberVM.Username.ToUpper());
+           
+
+            if(await _userManager.FindByNameAsync(memberVM.Username) != null)
+            {
+                ModelState.AddModelError("Username", "User alrady exist");
+                return View();
+            }
+            else if(await _userManager.FindByEmailAsync(memberVM.Email) != null)
+            {
+                ModelState.AddModelError("Email", "Email alrady exist");
+                return View();
+            }
+
+            AppUser user = new AppUser
+            {
+                UserName = memberVM.Username,
+                Email = memberVM.Email,
+                Fullname = memberVM.Fullname,
+            };
+           var result = await _userManager.CreateAsync(user, memberVM.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach(var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+
+                return View();
+            }
+
+            return RedirectToAction("login");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(MemberLoginViewModel memberVM, string returnUrl)
+        {
+            AppUser user = await _userManager.FindByNameAsync(memberVM.Username);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Username or Password is incorrect!");
+                return View();
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            if (!roles.Contains("Member"))
+            {
+                ModelState.AddModelError("", "Username or Passwrod is incorrect!");
+                return View();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, memberVM.Password, false, true);
+
+            if (result.IsLockedOut)
+            {
+                ModelState.AddModelError("", "5 deqiqe sonra yoxlayin");
+                return View();
+            }
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Username or Password is incorrect!");
+                return View();
+            }
+
+            if (returnUrl != null)
+                return Redirect(returnUrl);
+
+            return RedirectToAction("index", "home");
+        }
+        public async Task<IActionResult> Show()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+                return Content(user.Fullname);
+            }
+            return Content("logged out");
+        }
+
+        [Authorize(Roles = "Member")]
+        public async Task<IActionResult> Profile()
+        {
+            AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            MemberUpdateViewModel memberVM = new MemberUpdateViewModel
+            {
+                Username = user.UserName,
+                Fullname = user.Fullname,
+                Email = user.Email,
+            };
+            return View(memberVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Profile(MemberUpdateViewModel memberVM)
+        {
+            return Ok(memberVM);
+        }
+    }
+}
---
+++ _Pustok/Program.cs
@@ -0,0 +1,50 @@
+using _Pustok.DAL;
+using _Pustok.Models;
+using _Pustok.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PustokContext>(opt =>
+{
+    opt.UseSqlServer(@"SERVER=SAFAROFF\SQLEXPRESS01;Database=PustokApp;Trusted_Connection=TRUE");
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
+{
+    opt.Password.RequireDigit = false;
+    opt.Password.RequiredLength = 8;
+    opt.Password.RequireNonAlphanumeric = false;
+}).AddDefaultTokenProviders().AddEntityFrameworkStores<PustokContext>();
+builder.Services.AddScoped<LayoutService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+       name: "areas",
+       pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+   );
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
